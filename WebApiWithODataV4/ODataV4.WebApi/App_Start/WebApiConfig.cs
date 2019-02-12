92eee421f91f68ca00cef8ca4196e74b2d4508da
Raju Singh <raju.singh41@gmail.com>
2019-02-13 00:49:19 +0530

    add

--- WebApiWithODataV4/ODataV4.WebApi/App_Start/WebApiConfig.cs
+++ WebApiWithODataV4/ODataV4.WebApi/App_Start/WebApiConfig.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using Microsoft.AspNet.OData.Builder;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.OData;
+using Microsoft.OData.Edm;
+using ODataV4.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
@@ -9,16 +14,31 @@ namespace ODataV4.WebApi
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API configuration and services
+            //// Web API configuration and services
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
+            //// Web API routes
+            //config.MapHttpAttributeRoutes();
 
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+            //config.Routes.MapHttpRoute(
+            //    name: "DefaultApi",
+            //    routeTemplate: "api/{controller}/{id}",
+            //    defaults: new { id = RouteParameter.Optional }
+            //);
+
+            config.MapODataServiceRoute("ODataRout","Odata",GetEdmModel());
+            config.EnsureInitialized();
+
+        }
+
+        private static IEdmModel GetEdmModel()
+        {
+            var builder = new ODataConventionModelBuilder();
+            builder.Namespace = "ODataV4";
+            builder.ContainerName = "ODataV4Container";
+            builder.EntitySet<Person>("Pepole");
+            builder.EntitySet<Product>("Product");
+            return builder.GetEdmModel();
+
         }
     }
 }
